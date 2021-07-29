8065b96e219f1e2dfa64f1add08f46fe49fe066b
bdjm <brendandjmoore@gmail.com>
2021-07-29 21:09:31 +1000

    finding the project owner route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -19,5 +19,17 @@ async (req, res) => {
         });
     })
     .catch(err => console.log(err));
-    
-})
+
+    const OWNER = {
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    };
+
+    await Project.find({ owner: OWNER })
+    .then(projects => {
+        let finalArr = [...projects, ...projectsArr];
+        res.json(finalArr);
+    })
+    .catch(err => console.log(err));
+});
