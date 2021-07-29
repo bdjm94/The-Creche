ef58741fff8d13048f5df8b7d30a03cc71c7ae3f
bdjm <brendandjmoore@gmail.com>
2021-07-29 20:58:33 +1000

    adding start of the get route for the projects

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+
+const Project = require("../../models/Project");
+
+router.get("/",
+passport.authenticate("jwt", { session: false }),
+async (req, res) => {
+    let projectsArr = [];
+})
