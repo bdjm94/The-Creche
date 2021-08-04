b2a57892ca33220795522bd34f8a4eb3cf47a02e
bdjm <brendandjmoore@gmail.com>
2021-08-04 18:17:00 +1000

    adding validation checks for name, email and password input

--- validation/login.js
+++ validation/login.js
@@ -9,15 +9,15 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
 
 // Email checks
-  if (Validator.isEmpty(data.email)) {
+if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
+} else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
-  }
+}
 // Password checks
-  if (Validator.isEmpty(data.password)) {
+if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
-  }
+}
 
   return {
     errors,
