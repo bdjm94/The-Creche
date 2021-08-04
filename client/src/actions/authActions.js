bb62bb1e523e907b488b3c2be500e7c4c80a7763
bdjm <brendandjmoore@gmail.com>
2021-08-04 19:54:23 +1000

    registering user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -3,3 +3,16 @@ import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
 import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+    axios
+      .post("/api/users/register", userData)
+      .then(res => history.push("/"))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  };
