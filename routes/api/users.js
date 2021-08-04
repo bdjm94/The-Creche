bc74abf3f1ef5ebb2bd5345d2717c71e64b10191
bdjm <brendandjmoore@gmail.com>
2021-08-04 17:32:43 +1000

    adding the email register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,21 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 const User = require("../../models/User");
+
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
