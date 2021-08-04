05a7c0814bff563785d1ff0e304170e517b31994
bdjm <brendandjmoore@gmail.com>
2021-08-04 18:34:42 +1000

    adding the bodyparser middleware

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const projects = require("./routes/api/projects");
 const tasks = require("./routes/api/tasks");
 
 const app = express();
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
