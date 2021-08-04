debb92c906e5a89e79f526884f7a2a0b19a08486
bdjm <brendandjmoore@gmail.com>
2021-08-04 18:41:15 +1000

    updating the code

--- server.js
+++ server.js
@@ -39,13 +39,13 @@ app.use("/api/tasks", tasks);
 // Serve static assets (build folder) if in production
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static("client/build"));
+ app.use(express.static("client/build"));
   
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+ app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+ });
+}
   
-  const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
   
-  app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
