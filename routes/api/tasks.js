f1a3f9072480b80508e756c8bd3655bd1db78211
bdjm <brendandjmoore@gmail.com>
2021-08-04 17:03:50 +1000

    adding patch route to update tasks

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -37,3 +37,28 @@ passport.authenticate("jwt", { session: false }),
     });
   }
 );
+
+router.patch("/update",
+passport.authenticate("jwt", { session: false }),
+(req, res) => {
+    let taskFields = {};
+
+    taskFields.taskName = req.body.taskName;
+    if (req.body.dateDue && req.body.dateDue !== "Date undefined") {
+      taskFields.dateDue = req.body.dateDue;
+    }
+    taskFields.assignee = req.body.assignee;
+
+    Task.findOneAndUpdate(
+      { _id: req.body.id },
+      { $set: taskFields },
+      { new: true }
+    )
+      .then(task => {
+        res.json(task);
+      })
+      .catch(err => console.log(err));
+  }
+);
+
+module.exports = router;
