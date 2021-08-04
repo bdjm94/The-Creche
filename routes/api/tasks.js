f20997f9020f5a8fec266b9161f6b01567b82708
bdjm <brendandjmoore@gmail.com>
2021-08-04 16:49:36 +1000

    adding get by id route for tasks

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -3,3 +3,12 @@ const router = express.Router();
 const passport = require("passport");
 
 const Task = require("../../models/Task");
+
+router.get("/:id",
+passport.authenticate("jwt", { session: false }),
+(req, res) => {
+    let id = req.params.id;
+
+    Task.find({ project: id }).then(tasks => res.json(tasks));
+  }
+);
