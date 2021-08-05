85ba7f49212997e16004e711c165b959b28dd9fa
bdjm <brendandjmoore@gmail.com>
2021-08-05 20:14:18 +1000

    adding the create project type

--- client/src/reducers/projectsReducer.js
+++ client/src/reducers/projectsReducer.js
@@ -14,3 +14,11 @@ const initialState = {
   projectLoading: false,
   projectsLoading: false
 };
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_PROJECT:
+      return {
+        ...state,
+        projects: [action.payload, ...state.projects]
+      };
