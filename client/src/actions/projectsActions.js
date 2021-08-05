d9ec16636a0be949af32498308926328e3dcf37d
bdjm <brendandjmoore@gmail.com>
2021-08-05 19:00:18 +1000

    adding reducers to the project actions

--- client/src/actions/projectsActions.js
+++ client/src/actions/projectsActions.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+import {
+    CREATE_PROJECT,
+    UPDATE_PROJECT,
+    DELETE_PROJECT,
+    GET_PROJECT,
+    PROJECT_LOADING,
+    GET_PROJECTS,
+    PROJECTS_LOADING
+  } from "./types";
