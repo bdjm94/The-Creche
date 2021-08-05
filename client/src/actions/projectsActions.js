25161c41446ee3f3cf64fe25895697abe8667277
bdjm <brendandjmoore@gmail.com>
2021-08-05 19:03:45 +1000

    adding the create project route

--- client/src/actions/projectsActions.js
+++ client/src/actions/projectsActions.js
@@ -9,3 +9,16 @@ import {
     GET_PROJECTS,
     PROJECTS_LOADING
   } from "./types";
+
+  // Creating project
+export const createProject = projectData => dispatch => {
+  axios
+    .post("/api/projects/create", projectData)
+    .then(res =>
+      dispatch({
+        type: CREATE_PROJECT,
+        payload: res.data
+      })
+    )
+    .catch(err => console.log(err));
+};
