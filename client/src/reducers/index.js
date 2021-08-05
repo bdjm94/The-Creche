281289be5ee417257bf59fbe194053db01290732
bdjm <brendandjmoore@gmail.com>
2021-08-05 20:41:09 +1000

    adding all reducers

---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
+import projectsReducer from "./projectsReducer";
+import tasksReducer from "./tasksReducer";
