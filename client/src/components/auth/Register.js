5f9cac87d13039875b06a21cc3b16976a8d60b77
bdjm <brendandjmoore@gmail.com>
2021-08-08 01:40:55 +1000

    adding the constructor and effect hooks

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,3 +5,29 @@ import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 
 import "./Auth.scss";
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page then they will be redirected to the dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
