66afdbbd98a518c3eb5cb0727f0e627f51c213c6
bdjm <brendandjmoore@gmail.com>
2021-08-07 18:11:08 +1000

    adding the main content section to the tasks

--- client/src/components/dashboard/MainContent/Tasks.js
+++ client/src/components/dashboard/MainContent/Tasks.js
@@ -12,3 +12,36 @@ class Tasks extends Component {
   toggleModal = e => {
     this.setState({ modal: !this.state.modal });
   };
+
+    render() {
+    const { projects } = this.props.projects;
+
+    return (
+      <div className="main-content">
+        <h1 className="header">Your Tasks</h1>
+        <div className="projects">
+          <div className="no-projects">
+            <h1 className="header">You have no tasks</h1>
+            {projects.length > 0 ? (
+              <p>Visit a project to create your first task</p>
+            ) : (
+              <button className="main-btn" onClick={this.toggleModal}>
+                Create your first project
+              </button>
+            )}
+            <Modal onClose={this.toggleModal} modal={this.state.modal} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  projects: state.projects
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Tasks);
