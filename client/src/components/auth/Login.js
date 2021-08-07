8bf93390f7d0a524a657e7577a93628745f7179f
bdjm <brendandjmoore@gmail.com>
2021-08-08 01:46:52 +1000

    adding effect hooks and constructor to the login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,3 +5,25 @@ import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 
 import "./Auth.scss";
+
+class Login extends Component {
+    constructor() {
+      super();
+      this.state = {
+        email: "",
+        password: "",
+        errors: {}
+      };
+    }
+  
+    componentDidMount() {
+      // If logged in and user navigates to Login page then they will be redirected to the dashboard
+      if (this.props.auth.isAuthenticated) {
+        this.props.history.push("/dashboard");
+      }
+    }
+  
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.auth.isAuthenticated) {
+        this.props.history.push("/dashboard");
+      }
