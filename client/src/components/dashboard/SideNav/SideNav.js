aa5cf68ce70e30cf3decdd9bed047984afde84fe
bdjm <brendandjmoore@gmail.com>
2021-08-07 21:33:16 +1000

    adding function to hide the side navigation bar

--- client/src/components/dashboard/SideNav/SideNav.js
+++ client/src/components/dashboard/SideNav/SideNav.js
@@ -10,3 +10,18 @@ class SideNav extends Component {
     this.props.logoutUser(this.props.history);
     window.location.href = "/";
   };
+
+    // Hide the side navigation bar
+  toggleMenu = e => {
+    let sideNav = document.querySelector(".side");
+    sideNav.classList.add("invisibile");
+
+    let hamburger = document.querySelector(".hamburger-top-menu");
+    hamburger.classList.add("hamburger-visible");
+
+    let rightSide = document.querySelector(".right");
+    rightSide.classList.add("no-side");
+
+    let rightSideRight = document.querySelector(".right-top");
+    rightSideRight.classList.add("right-top-visibile");
+  };
