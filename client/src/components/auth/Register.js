32f90422218891e97e40e1e9a14d1bf32be60029
bdjm <brendandjmoore@gmail.com>
2021-08-07 23:20:42 +1000

    adding the imports to the register page

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+
+import "./Auth.scss";
