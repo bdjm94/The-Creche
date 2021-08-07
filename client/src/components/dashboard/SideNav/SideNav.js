d4d3ea2f6ceb2e16ad6969c3726b688722bc7966
bdjm <brendandjmoore@gmail.com>
2021-08-07 20:42:44 +1000

    adding the imports for the side navigation bar

---
+++ client/src/components/dashboard/SideNav/SideNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser } from "../../../actions/authActions";
+
+import "./SideNav.scss";
+
+class SideNav extends Component {
+  onLogoutClick = e => {
+    this.props.logoutUser(this.props.history);
+    window.location.href = "/";
+  };
