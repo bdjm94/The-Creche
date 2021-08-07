f7541cae824a7f54ed799c39147d808a0829e05e
bdjm <brendandjmoore@gmail.com>
2021-08-07 23:07:35 +1000

    adding the script for the 404 error page

--- client/src/components/404/404.js
+++ client/src/components/404/404.js
@@ -2,3 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./404.scss";
+
+const NotFound = props => {
+  return (
+    <div className="not-found">
+      <Link to="/dashboard">
+        <b>404</b>
+      </Link>
+      <br />
+      <p>The requested page was not found on our server.</p>
+      <p>
+        Either the URL you entered is incorrect, you do not have the
+        privileges to access this page, or the page you are looking for has been
+        removed.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
