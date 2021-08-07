a42d2d4a05e2c3721fe753b41111084f44d6a620
bdjm <brendandjmoore@gmail.com>
2021-08-07 17:08:00 +1000

    adding states

--- client/src/components/dashboard/MainContent/Dashboard.js
+++ client/src/components/dashboard/MainContent/Dashboard.js
@@ -5,3 +5,13 @@ import "./Dashboard.scss";
 import { connect } from "react-redux";
 
 import Modal from "./Modal/Modal";
+
+class Dashboard extends Component {
+  state = {
+    modal: false,
+    edit: false,
+    name: "",
+    members: [],
+    id: "",
+    owner: {}
+  };
