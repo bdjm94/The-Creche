c52bee820055f0a947956c05956264f02660b663
bdjm <brendandjmoore@gmail.com>
2021-08-09 23:00:58 +1000

    removing mongouri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://bdjm94:12veTrees@workouttracker-east.0i6p3.mongodb.net/crechedb?retryWrites=true&w=majority",
+  mongoURI: "",
   secretOrKey: "secret"
 };
