59a5d8fa1b568220d55a3bd432a3c180b7c90c44
bdjm <brendandjmoore@gmail.com>
2021-08-09 19:21:26 +1000

    updating app.listen

--- server.js
+++ server.js
@@ -43,4 +43,6 @@ if (process.env.NODE_ENV === "production") {
   
 const PORT = process.env.PORT || 3001;
   
-app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT} !`);
+});
